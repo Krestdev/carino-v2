a75ff3300f6fef5225b11fd7c77cf892add1fe8b
socrateNz <sssnzogning0@gmail.com>
2026-05-26 16:07:15 +0100

    product.price > 500

--- app/api/catalog/dishes/route.ts
+++ app/api/catalog/dishes/route.ts
@@ -6,7 +6,7 @@ import { ProductsData } from "@/types/types";
 export async function GET() {
     try {
         const data: { dishes: ProductsData[] } = await getProducts();
-        return NextResponse.json(data.dishes);
+        return NextResponse.json(data.dishes.filter(x => x.price > 500));
     } catch (error: any) {
         return NextResponse.json(
             { error: "Erreur lors de la récupération des produits" },
